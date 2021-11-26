042c18f3bef07e7e608628d07a996f831a2fee22
shigemasa yuki <yushin1301@gmail.com>
2021-11-26 23:26:28 +0900

    update convert_test

--- kadai1/cmd/convert_test.go
+++ kadai1/cmd/convert_test.go
@@ -1,6 +1,36 @@
 package convert_test
 
+import (
+	"convert"
+	"strconv"
+	"testing"
+	"fmt"
+)
 
-func TestConvert() {
-	
+
+func TestConvert(t *testing.T) {
+	testArgs := []struct {
+		Path  string
+		From string
+		To   string
+	} {
+		{Path: "../sample/test1.png", From: ".jpg", To: ".png"},
+		{Path: "../sample/test1.png", From: ".jpg", To: ".ping"},
+		{Path: "../sample/sample2/test3.png", From: ".png", To: ".jpg"},
+	}
+
+	for i, v := range testArgs {
+		t.Run("test"+strconv.Itoa(i), func(t *testing.T) {
+			HelperConvert(v.Path, v.From, v.To, t)
+		})
+		
+	}
+
+}
+
+func HelperConvert(Path string, From string, To string, t *testing.T) {
+	t.Helper()
+	if err := convert.Convert(Path, From, To); err != nil {
+		t.Errorf("failed test %#v", err)
+	}
 }
